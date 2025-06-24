094e1f5da7d6df9c5cd94eb540994cb2e7bf13b3
MGGodo <godinezg.miguel11@gmail.com>
2025-06-24 14:48:56 -0500

    Configuración de Docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
 def create_app():
+    load_dotenv() # Load the env variables
+
     # Create the Flask application instance
     app = Flask(__name__)
 
     # Conection with MongoDB
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://mongadmin:adminmongo.@localhost:27017/')
+    MONGO_URI = os.environ.get("MONGO_URI")
     client = MongoClient(MONGO_URI)
 
     # DB mongo-calc
-    app.config["MONGO"] = client["mongo-server"]
+    app.config["MONGO"] = client["mongo-calc"]
 
     # Import and register the calculator blueprint 
     from .routes.calculator import calculator_bp
